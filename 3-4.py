042041758236cd1ed9d54145a40586b77a577f8b
vietdinh672006-blip <vietdinh672006@gmail.com>
2025-12-06 09:34:08 +0700

    Add student information and print list

--- 3-4.py
+++ 3-4.py
@@ -1,5 +1,11 @@
+print("sinh vien : ho van viet")
+
+print("ma so sv :245751030110099")
+
+print("#############################")
 ds=input('danh sách :').split()
 print(ds)
 
 for so in ds:
     print(so)
+
