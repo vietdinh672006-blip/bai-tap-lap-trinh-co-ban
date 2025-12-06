a5aeeddca81a6e1fee97c8eb33e53a07c8db156a
vietdinh672006-blip <vietdinh672006@gmail.com>
2025-12-06 09:23:35 +0700

    Update 2-1.py

--- 2-1.py
+++ 2-1.py
@@ -1,6 +1,13 @@
+print("sinh vien : ho van viet")
+
+print("ma so sv :245751030110099")
+
+print("#############################")
+
 def sum(a,b):
     print ("sum="+str(a+b))
 #tinh tong 2 so 4,5
 sum(4,5)
 #tinh tong 2 so 3,7
 sum(3,7)
+
