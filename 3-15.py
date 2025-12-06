2373db2b2b72b505ca78171189eed00a55cecf5a
vietdinh672006-blip <vietdinh672006@gmail.com>
2025-12-06 09:36:11 +0700

    Update 3-15.py

--- 3-15.py
+++ 3-15.py
@@ -1,3 +1,8 @@
+print("sinh vien : ho van viet")
+
+print("ma so sv :245751030110099")
+
+print("#############################")
 s = input("Nhập chuỗi từ: ")
 words = s.split()
 
@@ -5,3 +10,4 @@ words.sort()
 
 for w in words:
     print(w)
+
