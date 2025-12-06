32ea84d537ac0aec13bcea634bf76a28009af8d9
vietdinh672006-blip <vietdinh672006@gmail.com>
2025-12-06 09:35:47 +0700

    Add student information and retain input functionality

--- 3-13.py
+++ 3-13.py
@@ -1,3 +1,9 @@
+print("sinh vien : ho van viet")
+
+print("ma so sv :245751030110099")
+
+print("#############################")
 ds = input("Nhập danh sách: ").split()
 
 print("Vị trí của chuỗi abc là:", ds.index('abc'))
+
