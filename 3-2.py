54c9b1a094163d4bc764dd8774381e71a56e4974
vietdinh672006-blip <vietdinh672006@gmail.com>
2025-12-06 09:33:31 +0700

    Update 3-2.py

--- 3-2.py
+++ 3-2.py
@@ -1,5 +1,12 @@
+print("sinh vien : ho van viet")
+
+print("ma so sv :245751030110099")
+
+print("#############################")
+
 S=input('nhap chuoi:')
 for ch in S :
     if ch == ' ' or ch =='\t':
         continue
     print(ch)
+
