66b95be180064364ef6470f13be65ed54854f81f
vietdinh672006-blip <vietdinh672006@gmail.com>
2025-12-06 09:16:00 +0700

    Update cau1.4.py

--- cau1.4.py
+++ cau1.4.py
@@ -1,9 +1,11 @@
-print("Sinh vien: LE TRONG TRUNG")
-print("Ma so SV : 245751030110070")
-print("#############################")
+print("sinh vien : ho van viet")
 
+print("ma so sv :245751030110099")
+
+print("#############################")
 i=1;
 for j in range(2,10):
     print("i:",i,"j:",j)
     print(i,"/",j)
     print (i/j);
+
