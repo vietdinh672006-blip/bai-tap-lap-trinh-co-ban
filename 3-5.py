5dff2e374e63fcd0d5c44ed8fc9f19ed2d19f687
vietdinh672006-blip <vietdinh672006@gmail.com>
2025-12-06 09:34:16 +0700

    Update 3-5.py

--- 3-5.py
+++ 3-5.py
@@ -1,3 +1,9 @@
+print("sinh vien : ho van viet")
+
+print("ma so sv :245751030110099")
+
+print("#############################")
 ds=input('danh sach:').split()
 for item in reversed(ds):
     print(item)
+
