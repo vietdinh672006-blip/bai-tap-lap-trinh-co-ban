c7a727ee72168bdd040cd024bd908746bcf38e8e
vietdinh672006-blip <vietdinh672006@gmail.com>
2025-12-06 09:24:28 +0700

    Refactor print statements and function definition

--- 2-4.py
+++ 2-4.py
@@ -1,6 +1,12 @@
+print("sinh vien : ho van viet")
+
+print("ma so sv :245751030110099")
+
+print("#############################")
 a="hello guy"
 def say(a):
     a="vinh university"
     print(a)
 say(a)
 print(a)
+
