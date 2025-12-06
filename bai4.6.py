ccdd537dcacc95b1928a1e25800fe5416b920507
vietdinh672006-blip <vietdinh672006@gmail.com>
2025-12-06 09:44:31 +0700

    Update bai4.6.py

--- bai4.6.py
+++ bai4.6.py
@@ -1,6 +1,12 @@
+print("sinh vien : ho van viet")
+
+print("ma so sv :245751030110099")
+
+print("#############################")
 hoten = input('Nhap ho va ten: ').strip()  
 ds = hoten.split()
 ho = ds[0]
 ten = ds[1]
 print('Ho:', ho)
 print('Ten rieng:', ten)
+
