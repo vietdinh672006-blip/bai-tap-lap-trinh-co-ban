364477f56fc9badecaec2256f33c0ad3c618ab22
vietdinh672006-blip <vietdinh672006@gmail.com>
2025-12-06 09:35:27 +0700

    Rearrange print statements and input handling

--- 3-11.py
+++ 3-11.py
@@ -1,6 +1,12 @@
+print("sinh vien : ho van viet")
+
+print("ma so sv :245751030110099")
+
+print("#############################") 
 ds = input("Nhập danh sách: ").split()
 
 ds.append('abc')      # thêm phần tử vào cuối list
 
 for ch in ds:
     print(ch)
+
