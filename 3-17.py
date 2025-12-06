ca24082f4a239695f384ba13a87697e1d1015d27
vietdinh672006-blip <vietdinh672006@gmail.com>
2025-12-06 09:36:30 +0700

    Refactor student info and divisor printing

--- 3-17.py
+++ 3-17.py
@@ -1,5 +1,12 @@
+print("sinh vien : ho van viet")
+
+print("ma so sv :245751030110099")
+
+print("#############################")
+
 n = int(input("Nhập n: "))
 
 for i in range(1, n+1):
     if n % i == 0:
         print(i)
+
