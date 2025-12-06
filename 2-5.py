8b829158ada02ed168173888a973b54e977d9084
vietdinh672006-blip <vietdinh672006@gmail.com>
2025-12-06 09:24:44 +0700

    Update 2-5.py

--- 2-5.py
+++ 2-5.py
@@ -1,3 +1,9 @@
+print("sinh vien : ho van viet")
+
+print("ma so sv :245751030110099")
+
+print("#############################")
+
 a="hello guy"
 def say ():
     global a
@@ -5,3 +11,4 @@ def say ():
     print(a)
 say()
 print(a)
+
