dffaa2a2b4266d98f61966a70eb37843761c2d60
vietdinh672006-blip <vietdinh672006@gmail.com>
2025-12-06 09:33:56 +0700

    Update 3-3.py

--- 3-3.py
+++ 3-3.py
@@ -1,4 +1,11 @@
+print("sinh vien : ho van viet")
+
+print("ma so sv :245751030110099")
+
+print("#############################")
+
 s=input('nhap chuoi:')
 for ch in s :
     print(ch.upper())
  
+
