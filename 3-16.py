b773abb3224f89587078d17c51216378b4e537a3
vietdinh672006-blip <vietdinh672006@gmail.com>
2025-12-06 09:36:21 +0700

    Add student information and rearrange input code

--- 3-16.py
+++ 3-16.py
@@ -1,5 +1,12 @@
+print("sinh vien : ho van viet")
+
+print("ma so sv :245751030110099")
+
+print("#############################")
+
 s = input("Nhập chuỗi nhị phân: ")
 lst = s.split(',')
 
 for val in lst:
     print(val)
+
