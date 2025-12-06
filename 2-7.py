517142a82a9c1d74028543a1d481105004396755
vietdinh672006-blip <vietdinh672006@gmail.com>
2025-12-06 09:25:05 +0700

    Update 2-7.py

--- 2-7.py
+++ 2-7.py
@@ -1,6 +1,12 @@
+print("sinh vien : ho van viet")
+
+print("ma so sv :245751030110099")
+
+print("#############################")
 def checkValue(n):
     if n%2==0:
         print("đâY là 1 SỐ chẵn")
     else:
      print("đây là 1 số lẻ")
 checkValue(7)
+
