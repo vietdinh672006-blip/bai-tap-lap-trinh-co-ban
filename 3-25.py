22a7a3efa240399a630024f35e012dbaf39937f5
vietdinh672006-blip <vietdinh672006@gmail.com>
2025-12-06 09:38:45 +0700

    Update 3-25.py

--- 3-25.py
+++ 3-25.py
@@ -1,3 +1,9 @@
+print("sinh vien : ho van viet")
+
+print("ma so sv :245751030110099")
+
+print("#############################")
+
 s = input("Nhập danh sách số (cách nhau bởi dấu phẩy): ")
 
 nums = [int(x) for x in s.split(',')]
@@ -5,3 +11,4 @@ nums = [int(x) for x in s.split(',')]
 odd = [str(x) for x in nums if x % 2 == 1]
 
 print(",".join(odd))
+
