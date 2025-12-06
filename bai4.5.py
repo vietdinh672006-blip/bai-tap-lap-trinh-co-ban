b7cd2d5f9828a78fa355f02c0ab61fa1c6104343
vietdinh672006-blip <vietdinh672006@gmail.com>
2025-12-06 09:44:24 +0700

    Update bai4.5.py

--- bai4.5.py
+++ bai4.5.py
@@ -1,5 +1,11 @@
+print("sinh vien : ho van viet")
+
+print("ma so sv :245751030110099")
+
+print("#############################")
 ds = input('Danh sach: ').split()   
 print(ds)                           
 for tu in reversed(ds):             
     print(tu)
     
+
