6b426303841a9df9dbc0de5901920d3dad6668f5
vietdinh672006-blip <vietdinh672006@gmail.com>
2025-12-06 09:34:26 +0700

    Update 3-6.py

--- 3-6.py
+++ 3-6.py
@@ -1,5 +1,11 @@
+print("sinh vien : ho van viet")
+
+print("ma so sv :245751030110099")
+
+print("#############################")
 hoten = input("Nhập họ tên: ").split()
 ho = hoten[0]
 ten = hoten[1]
 print("Họ:", ho)
 print("Tên:", ten)
+
