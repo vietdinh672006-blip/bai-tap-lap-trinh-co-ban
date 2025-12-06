93a494fdaf9df6ff4854c2d34c8d73a7f3ea3268
vietdinh672006-blip <vietdinh672006@gmail.com>
2025-12-06 09:34:47 +0700

    Update 3-8.py

--- 3-8.py
+++ 3-8.py
@@ -1,3 +1,9 @@
+print("sinh vien : ho van viet")
+
+print("ma so sv :245751030110099")
+
+print("#############################")
+
 ds = input("Nhập dãy từ: ").split()
 
 maxlen = max(len(t) for t in ds)
@@ -5,3 +11,4 @@ maxlen = max(len(t) for t in ds)
 for t in ds:
     if len(t) == maxlen:
         print(t)
+
