dfbdb28c9446c66e26c33e6c00519a5288330209
vietdinh672006-blip <vietdinh672006@gmail.com>
2025-12-06 09:38:27 +0700

    Update 3-23.py

--- 3-23.py
+++ 3-23.py
@@ -1,3 +1,9 @@
+print("sinh vien : ho van viet")
+
+print("ma so sv :245751030110099")
+
+print("#############################")
+
 s = input("Nhập câu: ")
 
 so_chu_cai = 0
@@ -11,3 +17,4 @@ for ch in s:
 
 print("Số chữ cái là:", so_chu_cai)
 print("Số chữ số là:", so_chu_so)
+
