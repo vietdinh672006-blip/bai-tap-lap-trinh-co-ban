c5dde787fd52ba10fb294ecf0509ba1b5fc2855f
vietdinh672006-blip <vietdinh672006@gmail.com>
2025-12-06 09:14:59 +0700

    Update student information and distance calculation

--- cau1.1.py
+++ cau1.1.py
@@ -1,7 +1,8 @@
-print("Sinh vien: LE TRONG TRUNG")
-print("Ma so SV : 245751030110070")
-print("#############################")
+print("sinh vien : ho van viet")
 
+print("ma so sv :245751030110099")
+
+print("#############################")
 n1=int(input("enter n1 value: "))
 n2=int(input("enter n2 value: "))
 if n1 > n2:
@@ -24,4 +25,5 @@ print ("Distance between two points:",res);
 
 
     
+
 
