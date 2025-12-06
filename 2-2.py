720b7716c43584eef2a9fb21a41ed1ecaa4d99c3
vietdinh672006-blip <vietdinh672006@gmail.com>
2025-12-06 09:23:57 +0700

    Refactor sum function and add student info prints

--- 2-2.py
+++ 2-2.py
@@ -1,4 +1,10 @@
+print("sinh vien : ho van viet")
+
+print("ma so sv :245751030110099")
+
+print("#############################")
 def sum(a,b):
  return a+b
 c=sum(4,5);
 print("tong cua 4 va 5="+str(c))
+
