c52e7ad6dd1d43b69de849e5df396169368ce2e9
vietdinh672006-blip <vietdinh672006@gmail.com>
2025-12-06 09:36:00 +0700

    Add student information and reorganize input code

--- 3-14.py
+++ 3-14.py
@@ -1,6 +1,12 @@
+print("sinh vien : ho van viet")
+
+print("ma so sv :245751030110099")
+
+print("#############################")
 ds = input("Nhập danh sách: ").split()
 
 ds.sort()     # sắp xếp theo thứ tự từ điển
 
 for ch in ds:
     print(ch)
+
