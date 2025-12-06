b093cd3ed95ca737fa7316020662ea78c684022a
vietdinh672006-blip <vietdinh672006@gmail.com>
2025-12-06 09:36:49 +0700

    Add student information and tuple creation

--- 3-19.py
+++ 3-19.py
@@ -1,2 +1,9 @@
+print("sinh vien : ho van viet")
+
+print("ma so sv :245751030110099")
+
+print("#############################")
+
 P = tuple(range(0, 1000001))
 print("Đã tạo tuple 0 → 1,000,000")
+
