636eeac2b2934aaf8d9312bb80dcaf226e014561
vietdinh672006-blip <vietdinh672006@gmail.com>
2025-12-06 09:35:35 +0700

    Update 3-12.py

--- 3-12.py
+++ 3-12.py
@@ -1,6 +1,12 @@
+print("sinh vien : ho van viet")
+
+print("ma so sv :245751030110099")
+
+print("#############################")
 ds = input("Nhập danh sách: ").split()
 
 ds.remove('123')      # xóa phần tử có giá trị '123'
 
 for ch in ds:
     print(ch)
+
