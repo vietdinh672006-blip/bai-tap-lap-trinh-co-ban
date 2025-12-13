c101b635c72258405338b4374162d2e0b5b678fd
vietdinh672006-blip <vietdinh672006@gmail.com>
2025-12-13 09:59:08 +0700

    Update cau1.9.py

--- cau1.9.py
+++ cau1.9.py
@@ -1,15 +1,16 @@
-print("sinh vien : ho van viet")
+     print("sinh vien : ho van viet")
 
-print("ma so sv :245751030110099")
-
-print("#############################")
-str=input("Enter a String:")
-dict = {}
-for n in str:
+     print("ma so sv :245751030110099")
+  
+      print("#############################")
+     str=input("Enter a String:")
+     dict = {}
+     for n in str:
     keys = dict.keys()
     if n in keys:
-        dict[n] += 1
-    else:
+         dict[n] += 1
+       else:
         dict[n] = 1
-print (dict)
+    print (dict)
+
 
